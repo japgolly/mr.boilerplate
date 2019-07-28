535841e23b93a40d75f3c5382fb8429dc417df5a
David Barri <japgolly@gmail.com>
2019-07-28 13:00:19 +1000

    Add a copy-to-clipboard button

--- webapp/src/main/scala/japgolly/mrboilerplate/webapp/OutputComponent.scala
+++ webapp/src/main/scala/japgolly/mrboilerplate/webapp/OutputComponent.scala
@@ -5,6 +5,7 @@ import japgolly.mrboilerplate.core.InputParser.Element.Unrecognised
 import japgolly.mrboilerplate.webapp.DataReusability._
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
+import org.scalajs.dom.{html, document}
 
 object OutputComponent {
 
@@ -15,38 +16,71 @@ object OutputComponent {
   implicit val reusabilityProps: Reusability[Props] =
     Reusability.derive
 
-  private def render(p: Props): VdomElement = {
+  final class Backend($: BackendScope[Props, Unit]) {
 
-    def failures =
-      Option.when(p.unrecognised.nonEmpty) {
-        <.div(
-          <.div("Failed to recognise:"),
-          <.pre(
-            ^.color.red,
-            ^.marginTop := "1em",
-            ^.marginLeft := "4ex",
-            p.unrecognised.iterator.map(_.text).mkString("\n\n")),
-          <.div(
-            ^.marginBottom := "1em",
-            "Ignoring the above, the rest of the input results in:"),
-        )
+    private val hiddenTextAreaRef = Ref[html.TextArea]
+
+    private val copyToClipboard: Callback =
+      for {
+        textArea <- hiddenTextAreaRef.get
+      } yield {
+        textArea.select()
+        document.execCommand("copy")
+        ()
       }
 
-    <.section(
-      Styles.outputOuter,
-      ^.display.flex,
-      ^.flexDirection.column,
-      <.header(Styles.header, "Output"),
-      failures,
-      <.div(
-        ^.overflow.auto,
-        ^.flexGrow := "1",
-        ^.minHeight := "16em",
-        Highlight(p.output)))
+    def render(p: Props): VdomElement = {
+
+      def failures =
+        Option.when(p.unrecognised.nonEmpty) {
+          <.div(
+            <.div("Failed to recognise:"),
+            <.pre(
+              ^.color.red,
+              ^.marginTop := "1em",
+              ^.marginLeft := "4ex",
+              p.unrecognised.iterator.map(_.text).mkString("\n\n")),
+            <.div(
+              ^.marginBottom := "1em",
+              "Ignoring the above, the rest of the input results in:"),
+          )
+        }
+
+      def copyButton = TagMod(
+        ^.position.relative,
+        <.textarea.withRef(hiddenTextAreaRef)(
+          ^.position.absolute,
+          ^.tabIndex := -1,
+          ^.zIndex := "-1",
+          ^.opacity := "0",
+          ^.readOnly := true,
+          ^.value := p.output),
+        <.button(
+          ^.position.absolute,
+          ^.top := "0",
+          ^.right := "0",
+          ^.cls := "btn btn-primary",
+          ^.role := "button",
+          ^.onClick --> copyToClipboard,
+          "Copy to clipboard"))
+
+      <.section(
+        Styles.outputOuter,
+        ^.display.flex,
+        ^.flexDirection.column,
+        <.header(Styles.header, "Output"),
+        failures,
+        <.div(
+          ^.overflow.auto,
+          ^.flexGrow := "1",
+          ^.minHeight := "16em",
+          copyButton,
+          Highlight(p.output)))
+    }
   }
 
   val Component = ScalaComponent.builder[Props]("OutputComponent")
-    .render_P(render)
+    .renderBackend[Backend]
     .configure(Reusability.shouldComponentUpdate)
     .build
 }
