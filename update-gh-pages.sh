1541be7b8c538676d73c41fd55e2c06b3647bd3f
David Barri <japgolly@gmail.com>
2019-07-28 13:18:59 +1000

    Add update-gh-pages.sh

---
+++ update-gh-pages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+cd "$(dirname "$0")"
+
+files=(
+  webapp/src/main/html/index.html
+  webapp/target/scala-2.12/mr-boilerplate.js
+  webapp/target/scala-2.12/mr-boilerplate.js.map
+)
+
+for f in ${files[@]}; do
+  rm -f $(basename $f)
+done
+
+sbt webapp/fullOptJS
+
+for f in ${files[@]}; do
+  cp $f .
+  git add $(basename $f)
+done
+
+git status
+
+echo "git commit -m 'Update gh-pages'"
+echo
+
